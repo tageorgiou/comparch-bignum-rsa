f79c866d685c9658bce13b8c7c9363905160ad50
Thomas Georgiou <2010tgeorgio@deneb.sun.tjhsst.edu>
2008-04-26 09:21:25 -0400

    Fixes

--- bignum.asm
+++ bignum.asm
@@ -1,20 +1,47 @@
 %define size 8
 global add
+global not
+global inc
 section .text
 add:
 	push ebp
 	mov ebp,esp
 	mov ebx,[ebp+8] ;a
 	mov edx,[ebp+12] ;b 
-	mov al,[ebx+7]
-	add al,[edx+7]
-	mov [ebx+7],al
-
-	mov al,[ebx+6]
-	adc al,[edx+6]
-	mov [ebx+6],al
-
+	mov ecx,size
+.loop:
+	mov al,[ebx+ecx-1]
+	adc al,[edx+ecx-1]
+	mov [ebx+ecx-1],al
+	loop .loop
 	mov eax,ebx
-	
+	pop ebp
+	ret
+inc:
+	push ebp
+	mov ebp,esp
+	mov ebx,[ebp+8] ;a
+	mov ecx,size
+	add byte [ebx+ecx-1],1
+	dec ecx
+.loop:
+	mov al,[ebx+ecx-1]
+	adc al,0
+	mov [ebx+ecx-1],al
+	loop .loop
+	mov eax,ebx
+	pop ebp
+	ret
+sub:
+
+mul:
+not:
+	push ebp
+	mov ebp,esp
+	mov eax,[ebp+8]
+	mov ecx,size
+.loop:
+	not byte [eax+ecx-1]
+	loop .loop
 	pop ebp
 	ret
